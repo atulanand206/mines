326d7ec796cca0134302bac5d3c021ef42f36788
Atul Anand <atulanand206@gmail.com>
2021-05-27 02:06:33 +0530

    UX: Refined behaviour

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import './Login.scss';
 import Logo from './../mines/Header/Logo';
+import { login } from './../mines/Server';
 
 interface State {
 	loginPage: boolean;
+	formError: boolean;
+	loggedIn: boolean;
+	username: string;
+	password: string;
+	rePassword: string;
+	email: string;
 }
 
 interface Props {
@@ -15,17 +23,56 @@ class Login extends React.Component<Props, State> {
 
 	constructor(props: Props) {
 		super(props);
-		this.state = { loginPage: true };
+		this.state = { 
+			loginPage: true, 
+			formError: false,
+			loggedIn: false,
+			username: '',
+			password: '',
+			rePassword: '',
+			email: ''
+		};
+		sessionStorage.setItem('access_token', '');
+		sessionStorage.setItem('refresh_token', '');
+	}
+
+	change(event: React.ChangeEvent<HTMLInputElement>, type: string) {
+		this.setState({formError: false})
+		switch(type) {
+			case 'username': this.setState({username: event.target.value}); break;
+			case 'password': this.setState({password: event.target.value}); break;
+			case 'rePassword': this.setState({rePassword: event.target.value}); break;
+			case 'email': this.setState({email: event.target.value}); break;
+		}
+	}
+
+	handle(event: React.MouseEvent<HTMLParagraphElement, MouseEvent>, loginPage: boolean) {
+		event.preventDefault();
+		if (loginPage === false) {
+			login({username: this.state.username, password:this.state.password})
+			.then((res) => {
+				if (res.status === 500) {
+					this.setState({formError: true})
+				}
+				if (res !== undefined) {
+					this.setState({loggedIn: true})
+				}
+			});
+		}
 	}
 
 	takeAction(loginPage: boolean) {
-		console.log(loginPage);
-		this.setState({ loginPage: loginPage });
+		this.setState({ loginPage: loginPage, formError: false });
 	}
 
 	render() {
+		const loggedIn = this.state.loggedIn;
+		if (loggedIn) {
+			return <Redirect to='/match' />
+		}
 		const login = this.state.loginPage;
 		const signup = !this.state.loginPage;
+		const error = this.state.formError;
 		return (
 			<div className='login-page'>
 				<div className='login-page-logo'>
@@ -36,24 +83,27 @@ class Login extends React.Component<Props, State> {
 						'form form-login',
 						signup && 'form__hidden')}>
 						<label className='form-input-label'>Username</label>
-						<input type='text' placeholder='Username' aria-label='username' />
+						<input type='text' placeholder='Username' aria-label='username' onChange={(e) => this.change(e, 'username')} />
 						<label className='form-input-label'>Password</label>
-						<input type='password' placeholder='Password' aria-label='password' />
-						<input type='submit' className='action-button' value='Login' />
+						<input type='password' placeholder='Password' aria-label='password' onChange={(e) => this.change(e, 'password')} />
+						<p className={classNames(
+							!error && 'form-error-message__hidden', 
+							error && 'form-error-message')}>Invalid credentials</p>
+						<input type='submit' className='action-button' value='Login' onClick={(e) => this.handle(e, false)} />
 						<p className={classNames('sub-action-button')} onClick={() => this.takeAction(false)} >Let's create a new account</p>
 					</form>
 					<form className={classNames(
 						'form form-sign-up',
 						login && 'form__hidden')}>
 						<label className='form-input-label'>Username</label>
-						<input type='text' placeholder='Set username' aria-label='username' />
+						<input type='text' placeholder='Set username' aria-label='username' onChange={(e) => this.change(e, 'username')} />
 						<label className='form-input-label'>Email Address</label>
-						<input type='email' placeholder='Set email address' aria-label='email' />
+						<input type='email' placeholder='Set email address' aria-label='email' onChange={(e) => this.change(e, 'email')} />
 						<label className='form-input-label'>Password</label>
-						<input type='password' placeholder='Set password' aria-label='password' />
+						<input type='password' placeholder='Set password' aria-label='password' onChange={(e) => this.change(e, 'password')} />
 						<label className='form-input-label'>Re enter password</label>
-						<input type='password' placeholder='Confirm password' aria-label='password' />
-						<input type='submit' className='action-button' value='Sign Up' />
+						<input type='password' placeholder='Confirm password' aria-label='password' onChange={(e) => this.change(e, 'rePassword')} />
+						<input type='submit' className='action-button' value='Sign Up' onClick={(e) => this.handle(e, true)} />
 						<p className={classNames('sub-action-button')} onClick={() => this.takeAction(true)} >I already have an account</p>
 					</form>
 				</div>
--- src/mines/Server.jsx
+++ src/mines/Server.jsx
@@ -2,9 +2,29 @@ import * as State from './State/State';
 
 const Bearer = "Bearer "
 
+export async function login(credentials) {
+    var response = await fetch(`http://${process.env.REACT_APP_AUTH}/authorize`, {
+        method: "POST",
+        body: JSON.stringify(credentials)
+    })
+    console.log(response.status)
+    switch (response.status) {
+        case 200: response = response.json(); break;
+        case 500: response = {status: response.status, body: "Request failed"}; break;
+        default: ;
+    }
+    console.log(response)
+    const json = response;
+    if (json.access_token !== null)
+        sessionStorage.setItem('access_token', json.access_token);
+    if (json.refresh_token !== null)
+        sessionStorage.setItem('refresh_token', json.refresh_token);
+    return json;
+}
+
 export function fetchGames(config, callback) {
     fetch(`http://${process.env.REACT_APP_URL}/games?config=${config}`, {
-            headers: {Authorization: Bearer + sessionStorage.getItem('token')}
+            headers: {Authorization: Bearer + sessionStorage.getItem('access_token')}
         })
         .then(response => response.json())
         .then(data => {
@@ -15,7 +35,7 @@ export function fetchGames(config, callback) {
 class Server {
     fetchBoard(config, callback) {
         fetch(`http://${process.env.REACT_APP_URL}/game/new?rows=${config.row}&columns=${config.col}&mines=${config.mines}`, {
-            headers: {Authorization: Bearer + sessionStorage.getItem('token')}
+            headers: {Authorization: Bearer + sessionStorage.getItem('access_token')}
         })
         .then(response => response.json())
         .then(data => {
@@ -27,7 +47,7 @@ class Server {
         fetch(`http://${process.env.REACT_APP_URL}/game/save`, {
             method: "POST", 
             body: JSON.stringify(game),
-            headers: {Authorization: Bearer + sessionStorage.getItem('token')}
+            headers: {Authorization: Bearer + sessionStorage.getItem('access_token')}
         }).then(res => {
             console.log("Request complete! response:", res);
         });
